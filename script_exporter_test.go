361ba70ef18461cba1ebc01f40585531a5dd2daf
Stephen O'Dwyer <67125983+angrieralien@users.noreply.github.com>
2023-04-27 14:55:53 -0400

    updates tests

--- script_exporter_test.go
+++ script_exporter_test.go
@@ -6,13 +6,13 @@ import (
 
 var config = &Config{
 	Metrics: map[string]*Metric{
-		"fake_metric": {"last-modify-time", "gauge", "help", []string{}, "namespace", struct{}{}},
+		"fake_metric": {"last-modify-time", "GaugeVec", "help", []string{}, "namespace", struct{}{}},
 	},
 	Scripts: []*Script{
 		{"success", "exit 0", 1, 1},
 		{"failure", "exit 1", 1, 1},
 		{"timeout", "sleep 5", 2, 1},
-		{"labels", "echo NAME:MYMETRIC:398493840\n", 1, 1},
+		{"labels", "echo NAME:MYMETRIC:LABEL_VALUES:398493840:RESULT:1\n", 1, 1},
 	},
 }
 
@@ -52,56 +52,4 @@ func TestRunScripts(t *testing.T) {
 		}
 
 	}
-}
-
-func TestScriptFilter(t *testing.T) {
-	t.Run("RequiredParameters", func(t *testing.T) {
-		_, err := scriptFilter(config.Scripts, "", "")
-
-		if err.Error() != "`name` or `pattern` required" {
-			t.Errorf("Expected failure when supplying no parameters")
-		}
-	})
-
-	t.Run("NameMatch", func(t *testing.T) {
-		scripts, err := scriptFilter(config.Scripts, "success", "")
-
-		if err != nil {
-			t.Errorf("Unexpected: %s", err.Error())
-		}
-
-		if len(scripts) != 1 || scripts[0] != config.Scripts[0] {
-			t.Errorf("Expected script not found")
-		}
-	})
-
-	t.Run("PatternMatch", func(t *testing.T) {
-		scripts, err := scriptFilter(config.Scripts, "", "fail.*")
-
-		if err != nil {
-			t.Errorf("Unexpected: %s", err.Error())
-		}
-
-		if len(scripts) != 1 || scripts[0] != config.Scripts[1] {
-			t.Errorf("Expected script not found")
-		}
-	})
-
-	t.Run("AllMatch", func(t *testing.T) {
-		scripts, err := scriptFilter(config.Scripts, "success", ".*")
-
-		if err != nil {
-			t.Errorf("Unexpected: %s", err.Error())
-		}
-
-		if len(scripts) != 4 {
-			t.Fatalf("Expected 3 scripts, received %d", len(scripts))
-		}
-
-		for i, script := range config.Scripts {
-			if scripts[i] != script {
-				t.Fatalf("Expected script not found")
-			}
-		}
-	})
 }
